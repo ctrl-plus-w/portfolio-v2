ab4cc37dc0392c1d81f4fe14c322563e8bb2bebf
ctrl-plus-w <lukas.ldrn@gmail.com>
2022-01-09 14:46:03 +0100

    🚀 Project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className='w-full h-full'>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
