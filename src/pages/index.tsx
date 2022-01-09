664fbf5ad09cb4bc23d46f03635c08140a1f78f1
ctrl-plus-w <lukas.ldrn@gmail.com>
2022-01-09 20:14:21 +0100

    ⚙️ Made a waiting screen (TODO) and added the Cards to the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import Head from 'next/head';
-
 import type { NextPage } from 'next';
+
+import { createRef, useEffect, useState } from 'react';
+import { gsap } from 'gsap';
+
+import Head from 'next/head';
 
 import BackgroundLine from '@element/BackgroundLine';
 import TalkButton from '@element/TalkButton';
@@ -9,7 +12,33 @@ import Text from '@element/Text';
 
 import Sparkles from '@icon/Sparkles';
 
+import Cards from '@module/Cards';
+
 const Home: NextPage = () => {
+  const waitingScreen = createRef<HTMLDivElement>();
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loaded || !waitingScreen.current) return;
+
+    let tl = gsap.timeline();
+
+    tl.to(waitingScreen.current, {
+      opacity: '0',
+      duration: 0.3,
+    }).to(waitingScreen.current, {
+      pointerEvents: 'none',
+      duration: 0,
+    });
+
+    tl.play();
+
+    return () => {
+      tl.kill();
+    };
+  }, [loaded, waitingScreen]);
+
   return (
     <div className='relative'>
       {/* ---------- PAGE SETTINGS --------- */}
@@ -17,13 +46,16 @@ const Home: NextPage = () => {
         <title>Lukas Laudrain</title>
       </Head>
 
+      {/* ---------- WAITING SCREEN -------- */}
+      <div className='fixed z-40 w-screen h-screen bg-dark-blue' ref={waitingScreen}></div>
+
       {/* ---------- LINES ----------------- */}
       <BackgroundLine width='50%' id={1} className='absolute top-0 left-0' />
       <BackgroundLine width='40%' id={2} className='absolute top-[20vh] right-0' />
 
       {/* ---------- HERO SECTION ---------- */}
-      <section id='hero-section' className='flex flex-col h-screen px-32 py-16'>
-        <div className='flex flex-col mt-32 h-full'>
+      <section id='hero-section' className='flex flex-row h-screen px-32 py-16'>
+        <div className='flex flex-col mt-32'>
           <Title sep>Lukas Laudrain</Title>
           <Text big>
             I am the french <b>full-stack developer</b>,<br /> that will help you <Sparkles />
@@ -35,6 +67,8 @@ const Home: NextPage = () => {
 
           <Text className='mt-auto'>© 2022 Lukas Laudrain</Text>
         </div>
+
+        <Cards loaded={loaded} setLoaded={setLoaded} />
       </section>
 
       <section id='about-me' className='flex flex-col'></section>
